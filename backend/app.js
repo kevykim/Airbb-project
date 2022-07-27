185a8c1bfa8408d73f34c7c94beaeba77cdbf8bc
Kevin Kim <kebonkim@gmail.com>
2022-07-27 13:59:53 -0700

    Add error-handling middleware for common errors and debugging

--- backend/app.js
+++ backend/app.js
@@ -5,14 +5,12 @@ const cors = require("cors");
 const csurf = require("csurf");
 const helmet = require("helmet");
 const cookieParser = require("cookie-parser");
-
+const { ValidationError } = require('sequelize');
 
 const { environment } = require('./config');
 const isProduction = environment === 'production';
 
-
 const app = express();
-
 
 app.use(morgan('dev'));
 app.use(cookieParser());
@@ -22,7 +20,6 @@ app.use(express.json());
 if(!isProduction) {
     app.use(cors());
 }
-
 app.use(helmet.crossOriginResourcePolicy({
     policy: "cross-origin"
 }));
@@ -40,6 +37,36 @@ const routes = require('./routes');
 
 app.use(routes);
 
+
+
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+});
+
+
+app.use((err, _req, _res, next) => {
+    if (err instanceof ValidationError) {
+        err.errors = err.errors.map((error) => error.message);
+        err.title = 'Validation error';
+    };
+    next(err);
+});
+
+
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+        title: err.title || 'Server Error',
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? null : err.stack
+    });
+});
 
 
 
