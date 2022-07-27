9a5e0b097ed5f97b0812d4cec48eb3c95d2c0bed
Kevin Kim <kebonkim@gmail.com>
2022-07-27 13:15:12 -0700

    auth backend

---
+++ backend/app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+require("express-async-errors");
+const morgan = require("morgan");
+const cors = require("cors");
+const csurf = require("csurf");
+const helmet = require("helmet");
+const cookieParser = require("cookie-parser");
+
+
+const { environment } = require('./config');
+const isProduction = environment === 'production';
+
+
+const app = express();
+
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(express.json());
+
+
+if(!isProduction) {
+    app.use(cors());
+}
+
+app.use(helmet.crossOriginResourcePolicy({
+    policy: "cross-origin"
+}));
+
+app.use(csurf({
+    cookie: {
+        secure: isProduction,
+        sameSite: isProduction && "Lax",
+        httpOnly: true
+    }
+}));
+
+
+const routes = require('./routes');
+
+app.use(routes);
+
+
+
+
+
+
+module.exports = app;
