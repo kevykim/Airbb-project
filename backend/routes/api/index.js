240bc1cb16fce2c73e930bf70a8cba1aedf5c5d4
Kevin Kim <kebonkim@gmail.com>
2022-07-28 14:31:04 -0700

    Add User logout backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sessionRouter = require("./session.js");
-const usersRouter = require("./users.js");
+const usersRouter = require("./user.js");
 const { restoreUser } = require("../../utils/auth.js");
 
 router.post('/test', (req, res) => {
