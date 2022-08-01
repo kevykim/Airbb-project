1298f78429cb6d8a3e8422f58dd390f6cbb389b1
Kevin Kim <kebonkim@gmail.com>
2022-08-01 13:16:57 -0700

    Review tables

---
+++ backend/db/migrations/20220801200639-create-review.js
@@ -0,0 +1,46 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Reviews", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      review: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      stars: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        unique: true,
+        references: { model: 'Users'}
+      },
+      spotId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        unique: true,
+        references: { model: 'Spots'}
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Reviews');
+  }
+};
---
+++ backend/db/models/review.js
@@ -0,0 +1,45 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Review extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Review.init({
+    review: { 
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    stars: { 
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        isNumeric: true,
+        min: 1,
+        max: 5
+      }
+    },
+    userId: { 
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      unique: true
+    },
+    spotId: { 
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      unique: true
+    },
+  }, {
+    sequelize,
+    modelName: 'Review',
+  });
+  return Review;
+};
