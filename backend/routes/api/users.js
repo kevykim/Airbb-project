e85c12e7d16c31cda5ce4999181fd2bb776726db
Kevin Kim <kebonkim@gmail.com>
2022-08-02 16:59:39 -0700

    Current user route??

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -33,6 +33,17 @@ const validateSignup = [
   handleValidationErrors,
 ];
 
+// Current User
+router.get("/current", requireAuth, async (req, res) => {
+  res.json({
+    id : req.user.id,
+    firstName: req.user.firstName,
+    lastName: req.user.lastName,
+    email: req.user.email,
+    // What is token?? 
+  })
+});
+
 // Sign up
 router.post('/', validateSignup, async (req, res) => {
     const { email, password, username, firstName, lastName } = req.body;
