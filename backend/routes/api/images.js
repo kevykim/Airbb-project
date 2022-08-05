2b50b2c9b507afda0f8f70c65a6eba1221862350
Kevin Kim <kebonkim@gmail.com>
2022-08-05 13:30:34 -0700

    image routes

---
+++ backend/routes/api/images.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const router = express.Router();
+
+const { requireAuth, restoreUser } = require("../../utils/auth");
+const { handleValidationErrors } = require("../../utils/validation");
+
+
+const { Spot, Image, Review, User, Booking, sequelize } = require("../../db/models");
+
+
+
+// Delete an Image
+
+router.delete('/:imageId', requireAuth, async (req, res) => {
+    const imageId = req.params.imageId
+
+    const image = await Image.findByPk(imageId)
+
+    if(!image) {
+        res.status(404)
+        res.json({
+            message: "Image couldn't be found",
+            statusCode: 404
+        })
+    };
+
+    res.status(200)
+    res.json({
+        message: "Successfully deleted",
+        statusCode: 200
+    });
+
+});
+
+
+
+
+
+
+
+
+
+module.exports = router
