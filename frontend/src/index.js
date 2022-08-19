0d3e75d8da6af7fdc53e777885b01bcb65698dec
Kevin Kim <kebonkim@gmail.com>
2022-08-18 20:48:12 -0700

    bleh

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,6 +11,8 @@ import configureStore from "./store";
 
 import { restoreCSRF, csrfFetch } from "./store/csrf";
 
+import * as sessionActions from "./store/session";
+
 
 const store = configureStore();
 
@@ -19,6 +21,7 @@ if (process.env.NODE_ENV !== "production") {
 
   window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
 
 
