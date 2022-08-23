02d3781afe75a0e7220904edbac45787f6ed5dcb
Kevin Kim <kebonkim@gmail.com>
2022-08-22 23:30:43 -0700

    spot simple CRUD fix

--- frontend/src/store/spots.js
+++ frontend/src/store/spots.js
@@ -36,6 +36,20 @@ const deleteSpot = (id) => {
 
 
 // THUNK ACTION CREATORS
+export const createSpots = (payload) => async dispatch => {
+    const response = await fetch('/api/spots', {
+        method: 'POST',
+        header: {'Content-Type':'application/json'},
+        body: JSON.stringify(payload)
+    })
+    if (response.ok) {
+        const data = await response.json()
+        dispatch(createSpot(data))
+        return data
+    } else {
+        return response.json()
+    }
+}
 
 export const getSpots = () => async dispatch => {
     const response = await fetch ('/api/spots')
@@ -44,6 +58,30 @@ export const getSpots = () => async dispatch => {
         const data = await response.json()
         dispatch(allSpots(data.Spots))
         // console.log('data',data.Spots)
+    }
+}
+
+export const editSpots = (payload) => async dispatch => {
+    const response = await fetch (`/api/spots/${payload.id}`, {
+        method: 'PUT',
+        header: {'Content-Type':'application/json'},
+        body: JSON.stringify(payload)
+    })
+    if (response.ok) {
+      const data = await response.json()
+      dispatch(editSpot(payload))
+      return data  
+    }
+}
+
+export const deleteSpots = (payload) => async dispatch => {
+    const response = await fetch(`/api/items/${payload}`, {
+        method: 'DELETE'
+    })
+    if (response.ok) {
+        const removal = await response.json()
+        dispatch(deleteSpot(payload))
+        return removal
     }
 }
 
