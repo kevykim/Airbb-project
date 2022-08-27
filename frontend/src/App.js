5b4bc45d88bd8b7a443db04d5d30d6a18a4c38a5
Kevin Kim <kebonkim@gmail.com>
2022-08-26 22:52:48 -0700

    fix route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
-import SignupFormPage from './components/SignUpFormPage'
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
 import SpotsHomePage from "./components/SpotsHomePage";
