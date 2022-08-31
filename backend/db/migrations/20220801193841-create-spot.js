c0c01d6d0a4193cc340d4b30bce64ad23645789d
Kevin Kim <kebonkim@gmail.com>
2022-08-30 23:33:55 -0700

    change

--- backend/db/migrations/20220801193841-create-spot.js
+++ backend/db/migrations/20220801193841-create-spot.js
@@ -50,9 +50,9 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
       },
-      previewImage: {
-        type: Sequelize.STRING,
-      },
+      // previewImage: {
+      //   type: Sequelize.STRING,
+      // },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
