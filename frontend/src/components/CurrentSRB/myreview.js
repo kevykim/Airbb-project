541647a82bb00fa401d78215d9386ddd6dd7b35c
Kevin Kim <kebonkim@gmail.com>
2022-09-18 20:21:19 -0700

    my spot css

--- frontend/src/components/CurrentSRB/myreview.js
+++ frontend/src/components/CurrentSRB/myreview.js
@@ -1,6 +1,9 @@
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { thunkAllCurrentReview } from "../../store/reviews";
+import ReviewsDeletePage from "../ReviewsDeletePage";
+import ReviewsUpdateModal from "../ReviewsUpdatePage/ReviewsUpdateModal";
+
 
 
 function MyReview() {
@@ -20,9 +23,43 @@ function MyReview() {
             <div>{reviews.map(review => (
                 <div key={review.id}>
                     <div>{review.review}</div>
+                    <div>{review.stars}</div>
+                    <ReviewsUpdateModal />
+                    <ReviewsDeletePage />
                 </div>
             ))}
             </div>
+              <div className="footer_container">
+          <div>
+            @ 2022 Airbb, Inc. &nbsp;·&nbsp;
+            <a
+              className="github_link"
+              href="https://github.com/kevykim"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Github
+            </a>
+            &nbsp;·&nbsp;
+            <a
+              className="linkedin_link"
+              href="https://linkedin.com/in/kevin-kim-a88429150"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              LinkedIn
+            </a>
+            &nbsp;·&nbsp;
+            <a
+              className="email_link"
+              href="mailto:kebonkim@gmail.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Email
+            </a>
+          </div>
+        </div>
         </div>
     )
 }
