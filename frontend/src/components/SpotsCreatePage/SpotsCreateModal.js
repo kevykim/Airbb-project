892fa5b11d126581a8c796d53e2c62cad8c1270f
Kevin Kim <kebonkim@gmail.com>
2022-09-19 21:39:25 -0700

    sign up??

--- frontend/src/components/SpotsCreatePage/SpotsCreateModal.js
+++ frontend/src/components/SpotsCreatePage/SpotsCreateModal.js
@@ -8,7 +8,7 @@ function SpotCreateModal() {
 
   return (
     <>
-      <button className="becomehostbutt" onClick={() => setShowModal(true)}>Become a Host</button>
+      <button className="becomehostbutton" onClick={() => setShowModal(true)}>Become a Host</button>
       {showModal && (
         <Modal onClose={() => setShowModal(false)}>
             <SpotsCreatePage onClick={(event) => setShowModal(false)} />
