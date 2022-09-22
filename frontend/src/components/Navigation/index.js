bba91266a2ab68a9b1d157a70ea80a5d1d0e1018
Kevin Kim <kebonkim@gmail.com>
2022-09-21 17:19:15 -0700

    change image and add icon airbb

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -12,34 +12,33 @@ import BecomeAHost from "../SignUpFormPage/BecomeHostModal.js";
 
 
 function Navigation({ isLoaded }) {
-
   const sessionUser = useSelector((state) => state.session.user);
-  const [startMenu, setStartMenu] = useState(false)
+  const [startMenu, setStartMenu] = useState(false);
 
+  // const openMenu = () => {
+  //   if (startMenu) return;
+  //   setStartMenu(true);
+  // };
 
-    // const openMenu = () => {
-    //   if (startMenu) return;
-    //   setStartMenu(true);
-    // };
+  // useEffect(() => {
+  //   if (!startMenu) return;
 
+  //   const closeMenu = (event) => {
+  //       setStartMenu(false);
+  //   };
 
-    // useEffect(() => {
-    //   if (!startMenu) return;
+  //   document.addEventListener("click", closeMenu);
 
-    //   const closeMenu = (event) => {
-    //       setStartMenu(false);
-    //   };
+  //   return () => document.removeEventListener("click", closeMenu);
+  // }, [startMenu]);
 
-    //   document.addEventListener("click", closeMenu);
-
-    //   return () => document.removeEventListener("click", closeMenu);
-    // }, [startMenu]);
+  //https://blog.logomyway.com/wp-content/uploads/2020/03/arbnb-logo.jpg
 
   let becomeHost;
   if (!sessionUser) {
     becomeHost = (
       <div>
-        <BecomeAHost showMenu={setStartMenu}/>
+        <BecomeAHost showMenu={setStartMenu} />
       </div>
     );
   }
@@ -50,7 +49,7 @@ function Navigation({ isLoaded }) {
       <div>
         <ProfileButton user={sessionUser} />
       </div>
-    );       
+    );
   } else {
     sessionLinks = (
       <div className="startbutton_container">
@@ -58,7 +57,10 @@ function Navigation({ isLoaded }) {
           <div style={{ marginRight: "10px" }} className="">
             {becomeHost}
           </div>
-          <button className="startbutton" onClick={ () => setStartMenu(banana => !banana)}>
+          <button
+            className="startbutton"
+            onClick={() => setStartMenu((banana) => !banana)}
+          >
             <i
               className="fa-solid fa-bars fa-2xl"
               style={{ color: "rgb(113, 113, 113", marginRight: "12px" }}
@@ -73,7 +75,7 @@ function Navigation({ isLoaded }) {
         {startMenu && (
           <div className="startmenu">
             <SignUpModal menu={startMenu} showMenu={setStartMenu} />
-              <LoginFormModal showMenu={setStartMenu} />
+            <LoginFormModal showMenu={setStartMenu} />
             <DemoUser />
           </div>
         )}
@@ -84,12 +86,14 @@ function Navigation({ isLoaded }) {
   return (
     <div className="navbar_container">
       <div className="navbar">
-        <div className="airbnbhome">
-          <NavLink exact to="/">
+        <div>
+          <NavLink className="airbnbhome" exact to="/">
             <img
-              src="https://blog.logomyway.com/wp-content/uploads/2020/03/arbnb-logo.jpg"
+              style={{ width: "40px", height: "40px" }}
+              src="favicon_io/apple-touch-icon.png"
               alt="Home"
             ></img>
+            <div className="airbnbhome_text">&nbsp;airbb</div>
           </NavLink>
         </div>
         {isLoaded && sessionLinks}
