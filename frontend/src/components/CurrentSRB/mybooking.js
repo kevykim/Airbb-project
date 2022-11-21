9dd6c4a074a8e6a71be159899cca92c417f6a2f5
Kevin Kim <kebonkim@gmail.com>
2022-11-20 21:58:10 -0800

    delete booking working

--- frontend/src/components/CurrentSRB/mybooking.js
+++ frontend/src/components/CurrentSRB/mybooking.js
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink, useHistory } from "react-router-dom";
 import { thunkGetCurrentBooking } from "../../store/bookings";
+import DeleteBooking from "../Bookings/DeleteBooking";
 
 import './mybooking.css'
 
@@ -58,7 +59,7 @@ function MyBooking() {
                     </NavLink>
                     
                     <div className="mybooking_buttons">
-                    
+                    <DeleteBooking spotId={booking.spotId} />
                     </div>
                   </div>
                 ))}
